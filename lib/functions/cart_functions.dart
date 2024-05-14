e080cdce8b873c803c6a7f7a2d177912908a1166
murshid-7 <147391448+murshid-7@users.noreply.github.com>
2024-05-14 15:32:31 +0530

    edit function and navigation

--- lib/functions/cart_functions.dart
+++ lib/functions/cart_functions.dart
@@ -3,24 +3,36 @@ import 'package:hive/hive.dart';
 import 'package:shoezo_app/models/cart_model.dart';
 
 ValueNotifier<List<CartModel>> cartListNotifier = ValueNotifier([]);
+double totalPrice = 0; // Moved totalPrice to the top level
+
 Future<void> addShoesCart(CartModel value) async {
   final shoeDB = await Hive.openBox<CartModel>('cart');
   await shoeDB.add(value);
-   
-  
   cartListNotifier.notifyListeners();
+  // Calculate total price after adding an item
+  calculateTotalPrice();
 }
-
 
 Future<void> getAllShoesCart() async {
   final shoeDB = await Hive.openBox<CartModel>('cart');
   cartListNotifier.value.clear();
   cartListNotifier.value.addAll(shoeDB.values);
+  // Calculate total price after fetching items
+  calculateTotalPrice();
   cartListNotifier.notifyListeners();
 }
 
 Future<void> deleteShoesCart(index) async {
   final shoeDB = await Hive.openBox<CartModel>('cart');
   await shoeDB.deleteAt(index);
+  // Calculate total price after deleting an item
+  calculateTotalPrice();
   cartListNotifier.notifyListeners();
+}
+
+void calculateTotalPrice() {
+  totalPrice = 0; // Reset totalPrice
+  for (var item in cartListNotifier.value) {
+    totalPrice += double.parse(item.price);
+  }
 }
